1d3326d1d181ed0d2732e23687a70d4f55b65f85
Cris8791 <135622871+Cris8791@users.noreply.github.com>
2024-02-12 21:45:57 +0200

    commit4

--- src/components/cast/Cast.jsx
+++ src/components/cast/Cast.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styles from './Cast.module.css';
 
