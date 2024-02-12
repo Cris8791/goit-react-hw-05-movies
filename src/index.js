825c4c0d25b5bfc64b466a811f2364e9d83ad6c4
Cris8791 <135622871+Cris8791@users.noreply.github.com>
2024-02-12 21:17:16 +0200

    commit3

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-// Orice alt import necesar
 
 const container = document.getElementById('root');
 const root = createRoot(container);
